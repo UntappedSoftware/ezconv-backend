986bbefbf109c7ab6ad4eb7bfb7be37ec854f8de
UntappedSoftware <145937124+UntappedSoftware@users.noreply.github.com>
2025-08-27 19:23:32 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Update system packages
 sudo apt-get update
+
+# Install ffmpeg
 sudo apt-get install -y ffmpeg
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install yt-dlp in the virtual environment
 pip install --upgrade yt-dlp
